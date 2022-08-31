065fe99eca12ed4f9e8275ab218b3b9624aef668
Daniel Kuznetsov <85589967+DanilKuznet1@users.noreply.github.com>
2022-08-31 00:03:22 -0700

    Backend pagination implementation is done
    Used query on a request parameter

--- controllers/postControllers.js
+++ controllers/postControllers.js
@@ -1,5 +1,40 @@
 const Post = require("../models/Post");
 
 exports.getAllPosts = async (req, res) => {
-  res.send("Post Controller");
+  try {
+    // just creating a promise and then execute the data with await
+    let query = Post.find();
+
+    const page = parseInt(req.query.page) || 1; // everything in the query comes as a string
+    const pageSize = parseInt(req.query.limit) || 50;
+    const skip = (page - 1) * pageSize; // sipping no documents
+    const total = await Post.countDocuments();
+
+    const pages = Math.ceil(total / pageSize);
+
+    query = query.skip(skip).limit(pageSize);
+
+    if (page > pages) {
+      res.status(404).json({
+        status: "failed",
+        message: "No more posts to show",
+      });
+    }
+
+    const result = await query;
+
+    res.status(200).json({
+      status: "success",
+      count: result.length,
+      page,
+      pages,
+      data: result,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      status: "error",
+      message: "Server error",
+    });
+  }
 };
